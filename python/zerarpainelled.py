75428cb8d84e1bfaaa34b04cff784dc64e6be4f4
Douglas Esteves <douglas.brsoftware@gmail.com>
2017-08-08 13:31:55 -0300

    Create zerarpainelled.py

---
+++ python/zerarpainelled.py
@@ -0,0 +1,55 @@
+import socket
+import numpy as np
+import time
+
+UDP_IP = "IP"
+UDP_PORT = "port"
+
+HEAD_BYTE = 0
+HEAD_AMOUNT = 1
+
+message = [HEAD_BYTE, 0, 0, 0, 100]
+
+L = np.array([[0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+              [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+              [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+              [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+              [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+              [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+              [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+              [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+              [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+              [1, 0, 1, 0, 1, 0, 1, 0, 1, 0]])
+
+LHC = np.array([[0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+                [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+                [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+                [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+                [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+                [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+                [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+                [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+                [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+                [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]])
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+a = 0
+fullFrame = LHC
+while True:
+    time.sleep(0.25)
+    fullFrame = np.concatenate((fullFrame[:, 1:20], fullFrame[:, 0:1]), axis=1)
+    frame = fullFrame[:, 0:10]
+    frameArray = np.ravel(frame.T)
+    COLOR = [255, 255, 255]
+    image = []
+    for pixel in frameArray:
+        for color in COLOR:
+            image.append(pixel * color)
+
+    message += image
+    message = bytearray(message)
+    sock.sendto(message, (UDP_IP, UDP_PORT))
+
+    message = [HEAD_BYTE, 0, 0, 0, 100]
